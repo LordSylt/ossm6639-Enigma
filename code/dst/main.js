53dbcd50627adaf1a411d22135fb977fb2feb815
LordSylt <lordsylt@DESKTOP-RGF6P38.localdomain>
2025-08-18 18:01:36 +0200

    debugger setup

---
+++ code/dst/main.js
@@ -0,0 +1,6 @@
+"use strict";
+console.log("hello world");
+if (30 > 20) {
+    console.log("Hi again!");
+}
+//# sourceMappingURL=main.js.map
--- code/src/main.ts
+++ code/src/main.ts
@@ -1,1 +1,5 @@
 console.log("hello world");
+
+if (30 > 20) {
+    console.log("Hi again!")
+} 
